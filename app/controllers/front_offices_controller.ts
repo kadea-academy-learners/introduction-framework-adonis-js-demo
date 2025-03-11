9ca7ed28772a520afbfc9547f09e9ec369d43909
Randy-RM <randymuhema@gmail.com>
2025-03-11 14:45:28 +0100

    feat : add home page controller function

--- app/controllers/front_offices_controller.ts
+++ app/controllers/front_offices_controller.ts
@@ -1,4 +1,10 @@
-// import type { HttpContext } from '@adonisjs/core/http'
+import type { HttpContext } from '@adonisjs/core/http'
 
 export default class FrontOfficesController {
+  public async homePage({ view }: HttpContext) {
+    return view.render('pages/home', {
+      pageTitle: 'Wellcom to Blogoss kinshasa',
+      pageDescription: 'Blogoss the best trip blog in kinshasa',
+    })
+  }
 }
