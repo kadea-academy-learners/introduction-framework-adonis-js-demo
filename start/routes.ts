230d8f454789767ca69e3a6dffb53e94e91feea6
Randy-RM <randymuhema@gmail.com>
2025-03-11 14:57:36 +0100

    feat : add frontOffice controller routes

--- start/routes.ts
+++ start/routes.ts
@@ -8,5 +8,10 @@
 */
 
 import router from '@adonisjs/core/services/router'
+const FrontOfficesController = () => import('#controllers/front_offices_controller')
 
-router.on('/').render('pages/home')
+router.get('/', [FrontOfficesController, 'homePage']).as('home')
+router.get('/about', [FrontOfficesController, 'aboutPage']).as('about')
+router.get('/contact', [FrontOfficesController, 'contactPage']).as('contact')
+router.get('/services', [FrontOfficesController, 'servicesPage']).as('services')
+router.get('/blog', [FrontOfficesController, 'blogPage']).as('blog')
